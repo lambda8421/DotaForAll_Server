4d4e9805234abe3a79cd7aee3e1909b9006e9f87
lambda8421 <lambda8421@163.com>
2018-09-08 20:13:03 -0400

    add crawler service

---
+++ src/main/java/com/mofei/dota/crawler/CrawlerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.mofei.dota.crawler;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CrawlerServiceApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+//        HeroCrawler.
+    }
+}
