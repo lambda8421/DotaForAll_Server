acf04ed338e7fb1333f25bb05ef20270785da46b
lambda8421 <lambda8421@163.com>
2018-09-16 22:28:06 -0400

    CHANGE THE ENTITY

--- src/main/java/com/mofei/dota/web/entity/Hero.java
+++ src/main/java/com/mofei/dota/web/entity/Hero.java
@@ -28,11 +28,11 @@ public class Hero implements Serializable {
     @JsonProperty("attack_type")
     private AttackType attackType;
 
-   @ManyToMany
-   @JoinTable(name = "hero_role",
-           joinColumns = @JoinColumn(name = "hero_id"),
-           inverseJoinColumns = @JoinColumn(name = "role_id"))
-   @JsonProperty("roles")
+    @ManyToMany
+    @JoinTable(name = "hero_role",
+            joinColumns = @JoinColumn(name = "hero_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JsonProperty("roles")
     private List<Role> roles;
 
     @JsonProperty("legs")
