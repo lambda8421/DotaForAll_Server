8c3e4be9a2638a462285755ecfed396551f5fa5a
lambda8421 <lambda8421@163.com>
2018-09-16 22:30:41 -0400

    Add hero service

---
+++ src/main/java/com/mofei/dota/web/service/HeroService.java
@@ -0,0 +1,11 @@
+package com.mofei.dota.web.service;
+
+
+import com.mofei.dota.web.entity.Hero;
+
+import java.util.List;
+
+public interface HeroService {
+    List<Hero> getAllHeroes();
+
+}
---
+++ src/main/java/com/mofei/dota/web/service/impl/HeroServiceImpl.java
@@ -0,0 +1,17 @@
+package com.mofei.dota.web.service.impl;
+
+import com.mofei.dota.web.entity.Hero;
+import com.mofei.dota.web.service.HeroService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HeroServiceImpl implements HeroService {
+
+
+    @Override
+    public List<Hero> getAllHeroes() {
+        return null;
+    }
+}
