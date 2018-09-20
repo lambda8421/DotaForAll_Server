a2043997ed7f539b9ceaf6cc2512a96c3b3d7d39
lambda8421 <lambda8421@163.com>
2018-09-19 22:38:44 -0400

    Add new hero service method

--- src/main/java/com/mofei/dota/web/service/HeroService.java
+++ src/main/java/com/mofei/dota/web/service/HeroService.java
@@ -2,10 +2,14 @@ package com.mofei.dota.web.service;
 
 
 import com.mofei.dota.web.entity.Hero;
+import com.mofei.dota.web.entity.Role;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HeroService {
     List<Hero> getAllHeroes();
+    List<Hero> getAllHeroesByRoles(List<Role> roles);
+    Optional<Hero> getHeroById(Long id);
 
 }
--- src/main/java/com/mofei/dota/web/service/impl/HeroServiceImpl.java
+++ src/main/java/com/mofei/dota/web/service/impl/HeroServiceImpl.java
@@ -1,17 +1,33 @@
 package com.mofei.dota.web.service.impl;
 
 import com.mofei.dota.web.entity.Hero;
+import com.mofei.dota.web.entity.Role;
+import com.mofei.dota.web.repository.HeroRepository;
 import com.mofei.dota.web.service.HeroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HeroServiceImpl implements HeroService {
 
+    @Autowired
+    private HeroRepository heroRepository;
 
     @Override
     public List<Hero> getAllHeroes() {
-        return null;
+        return heroRepository.findAll();
+    }
+
+    @Override
+    public List<Hero> getAllHeroesByRoles(List<Role> roles) {
+        return heroRepository.findAllByRoles(roles);
+    }
+
+    @Override
+    public Optional<Hero> getHeroById(Long id) {
+        return heroRepository.findById(id);
     }
 }
